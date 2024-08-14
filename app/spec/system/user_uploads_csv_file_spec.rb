2e2dbaf0260943d83c4e1ae2f38ab871d5e701bc
Angelo Maia <request.angelo@gmail.com>
2024-08-14 17:37:09 -0300

    Corrige teste de upload com mock de Faraday

--- app/spec/system/user_uploads_csv_file_spec.rb
+++ app/spec/system/user_uploads_csv_file_spec.rb
@@ -3,6 +3,10 @@ require_relative '../../app'
 
 describe 'Usuário faz upload de um arquivo csv', js: true do
   it 'com sucesso' do
+    allow(Faraday).to receive(:new).and_return(
+      instance_double(Faraday::Connection, post: instance_double(
+                      Faraday::Response, status: 200, body: { success: true }.to_json)))
+
     visit '/'
     attach_file('csv-file', File.expand_path('../support/sample.csv', __dir__), make_visible: true)
     click_on 'Enviar'
